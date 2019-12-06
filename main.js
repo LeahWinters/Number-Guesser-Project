02f2b74233fb5f89471ddabbb4d82d09f751bd4a
Leah Winters <leah.winters24@gmail.com>
2019-12-05 20:03:33 -0700

    Made clear button work on all 4 inputs

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var clearButton = document.getElementById("clear-btn");
+var challengerForm = document.getElementById("gamer-form");
+var player1Name = document.getElementById("name-text1");
+var player1Guess = document.getElementById("guess-num1");
+var player2Name = document.getElementById("name-text2");
+var player2Guess = document.getElementById("guess-num2");
+
+challengerForm.addEventListener("keyup", checkInputFields);
+clearButton.addEventListener("click", clearInputs);
 
 function checkInputFields(){
-  var player1Name = document.getElementById("name-text1");
-  var player1Guess = document.getElementById("guess-num1");
-  var player2Name = document.getElementById("name-text2");
-  var player2Guess = document.getElementById("guess-num2");
   if(player1Name.value.length > 0 || player1Guess.value.length > 0 || player2Name.value.length > 0 || player2Guess.value.length > 0){
      clearButton.disabled = false;
       clearButton.style.backgroundColor = "#6e6e6e"
@@ -14,6 +18,6 @@ function checkInputFields(){
      }
 }
 
-var challengerForm = document.getElementById("gamer-form");
-
-challengerForm.addEventListener("keyup", checkInputFields);
+function clearInputs(){
+  player1Name.value = ""
+}
