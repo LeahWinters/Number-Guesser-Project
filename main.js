408026ced8bf1cbf9d93dcb67797e9d14c6e9515
Leah Winters <leah.winters24@gmail.com>
2019-12-06 17:48:20 -0700

    made clear text function

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function checkInputFields(){
   if(player1Name.value.length > 0 && player1Guess.value.length > 0 && player2Name.value.length > 0 && player2Guess.value.length > 0){
     submitButton.disabled = false;
     submitButton.style.backgroundColor = "#6e6e6e";
-    } else {
+    }else {
       submitButton.disabled = true;
       submitButton.style.backgroundColor = "#d0d2d3";
   }
@@ -34,15 +34,7 @@ function clearInputs(){
   player2Guess.value = "";
 }
 
-function submitGuess(e){
-  e.preventDefault();
-  console.log("heyyyy");
-  var nameOne = document.getElementById("name1");
-  nameOne.innerText = player1Name.value;
-  var numOne = document.getElementById("guessed-num1");
-  numOne.innerText = player1Guess.value;
-  var nameTwo = document.getElementById("name2");
-  nameTwo.innerText = player2Name.value;
-  var numTwo = document.getElementById("guessed-num2");
-  numTwo.innerText = player2Guess.value;
+function clearText(){
+  player1Guess.value = "";
+  player2Guess.value = "";
 }
